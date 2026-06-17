ed6468328eff7d95ad4fea032ed6594336efaec8
Michael Kuo <119911963+k9540213@users.noreply.github.com>
2026-06-16 20:19:15 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,13 @@
+public class Solution { // the class holds the solution
+    public boolean isAnagram(String s, String t) {  //the boolean function 
+        if (s.length() != t.length()) { // if  two strings are not the same length, return false
+            return false; // no need to keep going, return false right away
+        }
+
+        char[] sSort = s.toCharArray(); // convert the first string to an array 
+        char[] tSort = t.toCharArray(); // convert the second string to an array
+        Arrays.sort(sSort); // sort the first array 
+        Arrays.sort(tSort); // sort the second array 
+        return Arrays.equals(sSort, tSort); // return true if both sorted arrays are identical, otherwise return false
+    }
+}
